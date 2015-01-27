ca9feae072ffe61f387a66e5d1e340326ada401a
Michael Kutý <6du1ro.n@gmail.com>
2015-01-27 18:47:59 +0100

    dump release

--- roboticeclient/__init__.py
+++ roboticeclient/__init__.py
@@ -1,10 +1,9 @@
 
 __title__ = 'Robotice Client'
-__release__ = '4'
+__release__ = '5'
 __version__ = '0.3.%s' % __release__
 __author__ = 'Michael Kuty & Ales Komarek'
 __license__ = 'Apache 2.0'
 __copyright__ = ''
 
 
-from roboticeclient.client import Client  
